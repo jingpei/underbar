85be1df00b52c4aa9fc9acc03fef6cbaffa51e27
jingpei <jingjpei@gmail.com>
2015-06-24 22:22:05 -0400

    feature/map + pluck passed

--- src/underbar.js
+++ src/underbar.js
@@ -111,8 +111,13 @@
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
+    // like each(), but in addition to running the operation on9all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(x){
+      results.push(iterator(x));
+    })
+    return results;
   };
 
   /*
