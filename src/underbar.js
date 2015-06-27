7c0457098e54472dfcf6ab1618954aea0d4d2ff0
jingpei <jingjpei@gmail.com>
2015-06-27 19:58:32 -0400

    feature/every passed

--- src/underbar.js
+++ src/underbar.js
@@ -181,10 +181,15 @@
     }, false);
   };
 
-
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(prevMatch, item){
+      if(iterator === undefined){
+        return item
+      }
+      return prevMatch && !!iterator(item)
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
